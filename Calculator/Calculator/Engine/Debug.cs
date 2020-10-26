0cd65e6afaeead2fff65cd6a14197b054cd8d292
GreenData17 <GreenData17@users.noreply.github.com>
2020-10-26 17:17:03 +0100

    Update Debug.cs
    added time stamp

--- Calculator/Calculator/Engine/Debug.cs
+++ Calculator/Calculator/Engine/Debug.cs
@@ -4,8 +4,8 @@ namespace Calculator.Engine
 {
     public class Debug
     {
-        public static void LogCreate(string m) { Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine(" " + m); }
-        public static void LogDestroy(string m) { Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine(" " + m); }
-        public static void LogInfo(string m) { Console.ForegroundColor = ConsoleColor.Cyan; Console.WriteLine(m); }
+        public static void LogCreate(string m) { Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine(" " + $"[{DateTime.UtcNow.ToString("HH:mm:ss")}]  " + m); }
+        public static void LogDestroy(string m) { Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine(" " + $"[{DateTime.UtcNow.ToString("HH:mm:ss")}]  " + m); }
+        public static void LogInfo(string m) { Console.ForegroundColor = ConsoleColor.Cyan; Console.WriteLine($"[{DateTime.UtcNow.ToString("HH:mm:ss")}]  " + m); }
     }
 }
