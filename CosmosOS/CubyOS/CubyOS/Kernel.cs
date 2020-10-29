16dc7d999630f7fb07eefaded1a8038d674c4013
GreenData17 <GreenData17@users.noreply.github.com>
2020-10-29 18:52:21 +0100

    Added CosmosOS

---
+++ CosmosOS/CubyOS/CubyOS/Kernel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace CubyOS
+{
+    public class Kernel : Sys.Kernel
+    {
+        public static Kernel Instance;
+        public UserManager userManager = new UserManager();
+        public Sys.FileSystem.CosmosVFS fs;
+
+        public string CurrentDir = @"0:\";
+        public string CurrentUser = "";
+
+        protected override void BeforeRun()
+        {
+            Instance = this;
+            Console.WriteLine("CubyOS booted successfully.");
+            Console.WriteLine();
+
+            fs = new Sys.FileSystem.CosmosVFS();
+            Sys.FileSystem.VFS.VFSManager.RegisterVFS(fs);
+            
+            Console.Clear();
+        }
+
+        protected override void Run()
+        {
+            userManager.Login();
+            Console.Write("####");
+            Console.Read();
+            Sys.Power.Shutdown();
+        }
+    }
+}
